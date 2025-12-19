5f61979e6546a47b1a7c4aee23a8e84558cf0cb2
Adiytisuman24 <adiytisuman1@gmail.com>
2025-12-19 21:55:25 +0530

    Update Next.js and React to latest to fix vulnerability CVE-2025-66478 and resolve naming conflict in OverviewSection

--- components/dashboard/OverviewSection.tsx
+++ components/dashboard/OverviewSection.tsx
@@ -398,22 +398,3 @@ export function OverviewSection() {
     </div>
   );
 }
-
-function ChevronLeft(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="3"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m15 18-6-6 6-6" />
-    </svg>
-  )
-}
